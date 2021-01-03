d425362574558a635322aaacad59a9418016a03b
Kyle <kgibsonprogramming@gmail.com>
2021-01-03 15:07:25 -0500

    Added insertion sort to the search algorithms.
    Updated README to emphasize that these implementations are examples of
    integer sorting.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Algorithm and data structure examples, implemented in Python 3.
 
 These are basic examples of different algorithms and data structures, implemented in Python 3.
+
+Currently, all the search algorithms perform integer sorting only - these aren't intended as production-ready code, but as a reference for how these algorithms work.
--- src/Python_Portfolio_Main.py
+++ src/Python_Portfolio_Main.py
@@ -8,9 +8,9 @@ Various custom-coded data structures and algorithms
 
 import random as r
 
-sort_functions = []
-
 def bubble_sort(random_numbers):
+    # Bubble sort is highly inefficient and rarely used in practice
+    # Its main utility is a simple introduction to sorting algorithms
     do_sort = True
     while do_sort:
         do_sort = False # Reset for this round
@@ -18,14 +18,28 @@ def bubble_sort(random_numbers):
             if (n + 1) < len(random_numbers) and random_numbers[n] > random_numbers[n+1]:
                 random_numbers[n], random_numbers[n+1] = random_numbers[n+1], random_numbers[n]
                 do_sort = True
-    
     return str(random_numbers)
 
-def validate(user_choice):
+def insertion_sort(random_numbers):
+    # Insertion sort is both simple and efficient for small or mostly sorted lists
+    # It sees use in more complex sorting algorithms like Timsort
+    # Compared to selection sort - another simple sorting algorithm - insertion is
+    # usually the preferred choice due to fewer comparisons and its performance when
+    # data is already mostly sorted
+    i = 1
+    while i < len(random_numbers):
+        j = 1
+        while j > 0 and random_numbers[j-1] > random_numbers[j]:
+            random_numbers[j], random_numbers[j-1] = random_numbers[j-1], random_numbers[j]
+            j -= 1
+        i += 1
+    return str(random_numbers)
+    
+def validate(user_choice, sort_functions):
     is_valid = False
     try:
         val = int(user_choice)
-        if val > 0 and val < (len(sort_functions) + 1):
+        if val > 0 and val <= (len(sort_functions)):
             is_valid = True
     except ValueError:
         print("Please choose a valid number from the options table.")
@@ -34,15 +48,19 @@ def validate(user_choice):
 def main():
     r.seed(None)
     qty = 10
-    sort_functions.append(bubble_sort)
+    
+    sort_functions = [bubble_sort, insertion_sort]
     print("Select a sort option:")
     for i in range(len(sort_functions)):
         print(str(i + 1) + ") " + sort_functions[i].__name__)
     user_choice = input()
-    if(validate(user_choice)):
+    result = validate(user_choice, sort_functions)
+    if(result == True):
         random_numbers = r.sample(range(1,100), qty)
         print("Input: " + str(random_numbers))
         print("Output: " + sort_functions[int(i-1)](random_numbers))
+    else:
+        print("Invalid input; exiting...")
     
 
 if __name__ == "__main__":
